e16a0f9c2f7817a7eef2fe3c6a7c3ee0a63919c1
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-08-18 20:05:11 +0200

    Point generated dependencies to jitpack.io

--- hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
+++ hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
@@ -38,10 +38,10 @@ class HugoPlugin implements Plugin<Project> {
     }
 
     project.dependencies {
-      debugCompile 'com.jakewharton.hugo:hugo-runtime:1.2.2-SNAPSHOT'
+      debugCompile 'com.github.Turbo87.hugo:hugo-runtime:1.2.2'
       // TODO this should come transitively
       debugCompile 'org.aspectj:aspectjrt:1.8.6'
-      compile 'com.jakewharton.hugo:hugo-annotations:1.2.2-SNAPSHOT'
+      compile 'com.github.Turbo87.hugo:hugo-annotations:1.2.2'
     }
     project.extensions.create('hugo', HugoExtension)
   }
